3791b6f29616e8e8f83c3dab5434f726ebceddae
Ethan <pylearning9512@gmail.com>
2025-12-13 23:51:38 +0000

    Add testing print statements for gems
    Added print statements to test gem methods functionality and (future)  character selection.

--- CrystalGame/main.py
+++ CrystalGame/main.py
@@ -2,11 +2,11 @@ import time
 from gems import * 
 
 
-
+#testing how to print the available gems, this will be a character selection one day
 for k, v in gems.items():
     print(gems[v])
 
-
+#these print statements are testing the imported "gems.py" file by calling some methods between gems
 print(f'{jasper.attack(amethyst)}')
 time.sleep(5)
 
